8464e2a3dca6be3e91d205d6995628f1887a7508
damla <damlaoksel@hotmail.com>
2024-03-12 17:36:13 +0300

    Oyun butonları güncellendi

--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,21 @@
+
+/*
+ Ekrana random 5 kelime gelir
+ Kartlar doğru eşleşene kadar devam eder. => puan sistemi olacak
+ En çok yanlış yapılan kelimeler databasede tutulur. İstedikleri zaman bu kelimeler ile oynayabilirler => bu veriler için başka bir script yaz.
+ 
+ Oyunun ana sayfasında olacak butonlar;
+ BASLA butonundan sonra.
+ V1 = v1 ve türkçe anlamları
+ v2 = v1 , v2 ve türkçe anlamları anlamları
+ v3 = v1, v2 ve türkçe anlamları
+ En çok yanlış yapılan kelimeler butonu
+
+ süre sistemi olsun localstorage'te tut en son yazdır. her girdiğinde değişecek.
+*/
 let gameBtn=document.querySelector("#gameBtn");
+const buttonContainer = document.createElement('div');
+buttonContainer.classList.add('button-container');
 
 const clearMain = () => MAIN_CONTENT.innerHTML = '';
 const showIncompleteWords = () => {
@@ -12,15 +29,12 @@ const showIncompleteWords = () => {
       MAIN_CONTENT.style.alignItems = 'center';
       let counterWarning = document.createElement("h1");
       counterWarning.style.opacity = ".5";
-      counterWarning.innerHTML = 'Kayıtlı kelime bulunmamaktadır.';
+      counterWarning.innerHTML = 'Havuzda kelime bulunamadı. Lütfen havuza kelime ekleyin.';
       MAIN_CONTENT.appendChild(counterWarning);
     } else {
-      MAIN_CONTENT.style.height = '';
-      MAIN_CONTENT.style.justifyContent = '';
-      MAIN_CONTENT.style.alignItems = '';
       const sectionTitle = document.createElement("h1");
       sectionTitle.classList.add("section-title");
-      sectionTitle.innerHTML = 'Bilmediğim Kelimeler';
+      sectionTitle.innerHTML = 'PUAN GELECEK';
       const completeSection = document.createElement('section');
       completeSection.classList.add('complete-section');
   
@@ -30,15 +44,6 @@ const showIncompleteWords = () => {
       const thead = document.createElement('thead');
       const tbody = document.createElement('tbody');
   
-      const headerRow = document.createElement('tr');
-      headerRow.innerHTML = `
-        <th>V1</th>
-        <th>V2</th>
-        <th>V3</th>
-        <th>Türkçe Anlamı</th>
-      `;
-      thead.appendChild(headerRow);
-  
       incompleteWordsArray.forEach((word, index) => {
         const dataRow = document.createElement('tr');
         dataRow.innerHTML = `
@@ -47,11 +52,7 @@ const showIncompleteWords = () => {
           <td>${word.v3}</td>
           <td>${word.turkishMeaning}</td>
         `;
-  
-        if (index % 2 === 0) {
-          dataRow.style.backgroundColor = '#f2f2f2'; // Gri rengi
-        }
-  
+   
         tbody.appendChild(dataRow);
       });
   
@@ -61,10 +62,48 @@ const showIncompleteWords = () => {
       MAIN_CONTENT.appendChild(sectionTitle);
       MAIN_CONTENT.appendChild(completeSection);
     }
-  };
+};
+const gameButtons=()=>{
+  const gameTitle=document.createElement("h1");
+  gameTitle.textContent="IrregularVerbs";
+  buttonContainer.appendChild(gameTitle);
+
+  const startButton = document.createElement('button');
+  startButton.textContent = 'Başla';
+
+  startButton.addEventListener('click', levelDesign);
+  // startButton.addEventListener('click', startGame);
+
+  buttonContainer.appendChild(startButton);
+  MAIN_CONTENT.appendChild(buttonContainer);
+}
+const levelDesign=()=>{
+  buttonContainer
+  const v1Btn=document.createElement('button');
+  v1Btn.textContent="V1";
+  v1Btn.id="v1BTN";
+
+  const v2Btn=document.createElement('button');
+  v2Btn.textContent="V2";
+  v1Btn.id="v2BTN";
+
+  const v3Btn=document.createElement('button');
+  v3Btn.textContent="V3";
+  v1Btn.id="v3BTN";
+
+  buttonContainer.appendChild(v1Btn);
+  buttonContainer.appendChild(v2Btn);
+  buttonContainer.appendChild(v3Btn);
+
+  MAIN_CONTENT.appendChild(buttonContainer);
+
+}
+const startGame=()=>{
+  showIncompleteWords();
+}
 const game = () => {
     clearMain();      
-    showIncompleteWords();
+    gameButtons();
 }
 
 gameBtn.addEventListener("click",game);
